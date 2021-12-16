e929cbaec2851e384a67c65c73957e787a89cc8f
vishal kumar singh <vishurajputmgr@gmail.com>
2021-12-16 21:41:16 +0530

    contact and reponsive

--- asset/main.js
+++ asset/main.js
@@ -6,3 +6,12 @@ function myFunction() {
         document.getElementById("home").className = "";
     }
 }
+
+const menu = document.querySelectorAll('.menu')[0];
+const toggle = document.querySelectorAll('.toggle')[0];
+        
+
+        function toggleMenu() {
+            menu.classList.toggle('active');
+            toggle.classList.toggle('active');
+        };
